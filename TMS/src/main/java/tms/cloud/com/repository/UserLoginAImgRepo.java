6bd99016214f7d013ab37c09873a5dc3ae6a2aa1
Somanath Singh <110218220+singh10k@users.noreply.github.com>
2024-02-09 14:07:46 +0530

    Delete TMS directory

--- TMS/src/main/java/tms/cloud/com/repository/UserLoginAImgRepo.java
+++
@@ -1,9 +0,0 @@
-package tms.cloud.com.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import tms.cloud.com.model.LogImgDetails;
-
-public interface UserLoginAImgRepo extends CrudRepository<LogImgDetails, Integer> {
-
-}
